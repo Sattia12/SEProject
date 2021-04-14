6bcdc2e31eeb8358a3af3a6d033d3b7761828352
popetav <64201760+popetav@users.noreply.github.com>
2021-04-14 18:50:30 +0100

    Add files via upload

---
+++ AssessorLogin.php
@@ -0,0 +1,48 @@
+
+<!DOCTYPE html>
+<html>
+<head lang="en">
+<meta charset="utf-8">
+
+
+<title>login page</title>
+<link rel="stylesheet" href="login1.css" type="text/css">
+
+
+</head>
+<body>
+
+<header>
+
+<div class="header">
+   <h1>Log-in page</h1>
+</div>
+         
+</header>
+
+
+<div class = "form">
+      
+      <form role = "form" method = "GET">
+
+         <h4><?php echo $msg; ?></h4>
+         
+         <input type = "text"
+            name = "username" placeholder = "username" 
+            required autofocus></br>
+
+         <input type = "password"
+            name = "password" placeholder = "password" required>
+
+         <button class = "button" type = "submit" 
+            name = "login"><a href="assessorpage.html">Login</a></button>
+            
+      </form>
+         
+   
+      
+   </div> 
+ 
+ 
+</body>
+</html>
